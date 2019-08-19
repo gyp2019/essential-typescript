c2f30ad7889e04e26ddd0cc5fef17df1ef70f003
GimYoungPhil <youngphil.gim@gmail.com>
2019-08-19 17:15:12 +0900

    fixed

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-var todo: string = "Pick up drycleaning";
+var container = document.getElementById('container');
+
+function countdown(initial, final = 0, interval = 1) {
+
+  var current = initial;
+
+  while(current > final) {
+    container.innerHTML = current;
+    current -= interval;
+  }
+
+}
+
+
+countdown(10)
